c58dcb89df6192d58128713847195412b16c02e8
jacopo35 <jaczor00@gmail.com>
2021-12-16 15:44:08 +0100

    add-html&css

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/*
+Descrizione:
+Partendo dal markup allegato, rifare lo slider ma questa volta usando Vue.
+Bonus:
+1 - al click su uno dei pallini mostrare l’immagine in posizione corrispondente;
+2 - applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente;
+3 - quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce.
+*/
