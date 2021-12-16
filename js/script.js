2471b53a4029318173c7e4bbbcd068738d6fe051
jacopo35 <jaczor00@gmail.com>
2021-12-16 16:00:54 +0100

    add -js

--- js/script.js
+++ js/script.js
@@ -6,3 +6,32 @@ Bonus:
 2 - applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente;
 3 - quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce.
 */
+
+const app = new Vue(
+ {
+    el: '#app',
+    data: { 
+    images: [
+     'image1.jpg',
+     'image2.jpg',
+     'image3.jpg',
+     'image4.jpg'
+     ],
+    counter: 0
+    },
+    methods: { 
+    nextImg: function () {
+             this.counter += 1;
+             if (this.counter > this.images.length - 1) {
+                 this.counter = 0;
+             }
+            },
+    prevImg: function () {
+             this.counter -= 1;
+             if (this.counter < 0) {
+             this.counter = this.images.length - 1;
+             }
+            }
+        }
+    }
+)
